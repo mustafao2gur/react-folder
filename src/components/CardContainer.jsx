d67749f55117fc10d447d9664a2652f4a5fb8200
mustafao2gur <mustafaseviss@gmail.com>
2023-02-19 20:29:39 +0300

    son_1

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import PlayerCard from './PlayerCard'
-
-import {data} from "../helper/data" 
-
+import React from "react";
+import PlayerCard from "./PlayerCard";
+import { useState } from "react";
+import { data } from "../helper/data";
 
 const CardContainer = () => {
 
+  const [filteredList, setFilteredList] = useState("");
+const handelFilter =(e)=>{
+  setFilteredList(e.target.value)
+}
+
   return (
-    <div className='res'>
+    <div className="player">
       <form className="form">
-        <input type="text" placeholder="Search player..." />
+        <input
+          type="text"
+          placeholder="Search player..."
+          onChange={handelFilter}
+        />
       </form>
-      <div className="p-card">
-     
-        {data.map((e, i) => {
-          return (
-            <PlayerCard data={e} key={i} />
-          );
-        })}
+
+      <div className="dis">
+        {data
+          .filter((e) => {
+            return e.name.toLowerCase().includes(filteredList.toLowerCase());
+          })
+          .map((d, i) => {
+            return <PlayerCard d={d} i={i} />;
+          })}
       </div>
     </div>
   );
-}
+};
 
-export default CardContainer
+export default CardContainer;
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,36 +1,39 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { FaBasketballBall } from "react-icons/fa";
 
-const PlayerCard = ({data}) => {
-  const [on,setOn]=useState(true)
-  const {img,name,statistics}=data
-  const handleClick =()=>{
-    setOn(!on)
-    setTimeout(()=>setOn(on),2000)
+const PlayerCard = ({ d }, ) => {
+  console.log(d);
+  const{img,name,statistics}=d
+  const [on, setOn] = useState(true);
 
 
-  }
+  const handleClick = () => {
+    setOn(!on);
+    setTimeout(() => setOn(on), 2000);
+  };
   return (
-    <div onClick={handleClick} className="card-ctn" >
-      <div  key={statistics} className='card'>
-        {on?(
+    <div onClick={handleClick} className="card-ctn">
+      <div key={statistics} className="card">
+        {on ? (
           <div className="img-ctn">
             <img src={img} alt="player" />
           </div>
-        ):(
-             <ul className="list">
-              {data.statistics.map((e,i)=>{
-                return <li key={i}>
-                  <FaBasketballBall/>{e}
+        ) : (
+          <ul className="list">
+            {statistics.map((e, i) => {
+              return (
+                <li key={i}>
+                  <FaBasketballBall />
+                  {e}
                 </li>
-              })}
-            </ul>
+              );
+            })}
+          </ul>
         )}
         <p>{name}</p>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default PlayerCard
+export default PlayerCard;
